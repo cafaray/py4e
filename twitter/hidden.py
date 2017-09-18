599bcf27e944fbbb195f2f4eb5b63ffe2f12a148
cafaray <cafaray@gft.com>
2017-09-18 17:53:57 -0500

    Creating twitter application

---
+++ twitter/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "wa7C6nPxYmKvxQs5ca6WBAWYM",
+            "consumer_secret": "DUK2Ovrji4m28ab9v4Y0jGVlXwtHXGVhOGo04Jq6cIP5ubj7o3",
+            "token_key": "2507106253-AUqfMTv5nhyankraiLYVINt6gRW9gNRRUtGMgFp",
+            "token_secret": "H4tXm2iTUIzHrV8Oot7COhZ5TOjjZE6Th8ybNT97MgeK9"}
