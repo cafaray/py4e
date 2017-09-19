386d9f91d603905395ad93f4d380566cf7c0f013
Alberto Farias Ayala <cafaray@gmail.com>
2017-09-18 22:45:09 -0500

    Update hidden.py

--- twitter/hidden.py
+++ twitter/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "wa7C6nPxYmKvxQs5ca6WBAWYM",
-            "consumer_secret": "DUK2Ovrji4m28ab9v4Y0jGVlXwtHXGVhOGo04Jq6cIP5ubj7o3",
-            "token_key": "2507106253-AUqfMTv5nhyankraiLYVINt6gRW9gNRRUtGMgFp",
-            "token_secret": "H4tXm2iTUIzHrV8Oot7COhZ5TOjjZE6Th8ybNT97MgeK9"}
+    return {"consumer_key": "wa7C6nPxY...6WBAWYM",
+            "consumer_secret": "DUK2Ovrji4...5ubj7o3",
+            "token_key": "250...253-AUqfM...GMgFp",
+            "token_secret": "H4tXm2iTUIzHr...h8ybNT97MgeK9"}
